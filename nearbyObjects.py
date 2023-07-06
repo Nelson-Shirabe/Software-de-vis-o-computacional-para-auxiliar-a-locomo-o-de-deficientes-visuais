92a68ccc077f008a25e9ad4baecac0105d287813
Nelson-Shirabe <nelsonshirabejr@gmail.com>
2023-07-06 00:17:38 -0300

    Depth map for the 4 quadrants

--- nearbyObjects.py
+++ nearbyObjects.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+import matplotlib.pyplot as plt 
 
 class nearby_objects():
     
@@ -8,28 +9,28 @@ class nearby_objects():
         self.min = 200
         self.max = 255
         
-    def warning(self, img):
+    def warning(self, img, cx, cy):        
         # Seleciona os objetos que estão dentro do range de aviso - Imagem é Binarizada
-        caution = cv.inRange(img, self.min, self.max) 
+        caution= cv.inRange(img, self.min, self.max)
         # Criar o contorno e Definir a Intensidade
-        caution, I = self.contour(caution, img)
+        caution, I = self.contour(caution, img, cx, cy)
         
         return caution, I
     
-    def contour(self, caution, img):
+    def contour(self, caution, img, cx, cy):
         # Faz uma copia da imagem para não alterar a original
         caution_copy = caution.copy()
         # Encontra os contornos da imagem binarizada
         counturs, hier = cv.findContours(caution_copy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
         
         # Desenha os contornos na imagem binarizada e efetua o calculo da intensidade de saída
-        Imax = -1
+        Imax = 0
         rect = np.zeros(4)
         for cnt in range(len(counturs)):
             # Calcula a posição do retangulo
             x, y, w, h = cv.boundingRect(counturs[cnt])
             # Calculo da intensidade de saída
-            I = self.intensity(x, y, w, h, img, caution_copy)
+            I = self.intensity(x, y, w, h, img, caution_copy, cx, cy)
             
             if I > Imax:
                 Imax = I
@@ -40,18 +41,15 @@ class nearby_objects():
                
         return caution_copy, Imax
     
-    def intensity(self, x, y, w, h, img, caution):
-        # Pegar o shape da imagem
-        img_shape = img.shape
-        
-        # Centro da imagem
-        img_cx = img_shape[1]/2
-        img_cy = img_shape[0]/2
+    def intensity(self, x, y, w, h, img, caution, cx, cy):       
+        img_shape = img.shape 
+        img_cx = cx
+        img_cy = cy
         
         # Distância do centro do retângulo ao centro da imagem
         d = np.sqrt(((x/2) - img_cx)**2 + ((y/2) - img_cy)**2)
         # Normalizada
-        d = d/(np.sqrt(img_cx**2 + img_cy**2))
+        d = d/(np.sqrt(img_shape[1]**2 + img_shape[0]**2))
         
         
         # Pegar a média de intensidade do objeto selecionado:
@@ -69,7 +67,7 @@ class nearby_objects():
         mean_object = soma/n
     
         # Intensidade de saída (Imax=255 e Imin=0)
-        I = mean_object/(d+1)
+        I = (mean_object/(d+1))*((n/(w*h)))
         
         return I 
         
@@ -80,14 +78,56 @@ class nearby_objects():
 if __name__ == "__main__":
     # Para Testes
     img = cv.imread("Amostra.png", cv.IMREAD_UNCHANGED)
+    img_shape = img.shape
     
     objects = nearby_objects()
-    objects = objects.warning(img)
+    # 1º Quadrante
+    caution1 = img[:(img_shape[0]//2), (img_shape[1]//2): ] 
+    cx = 0
+    cy = 0
+    caution1, I1 = objects.warning(caution1, cx, cy)
     
-    cv.namedWindow("Warning", cv.WINDOW_NORMAL)
+    # 2º Quadrante
+    caution2 = img[:(img_shape[0]//2), :(img_shape[1]//2) ] 
+    cx = img_shape[1]//2
+    cy = 0
+    caution2, I2 = objects.warning(caution2, cx, cy)
     
+    # 3º Quadrante
+    caution3 = img[(img_shape[0]//2):, :(img_shape[1]//2) ] 
+    cx = img_shape[1]//2
+    cy = img_shape[0]//2
+    caution3, I3 = objects.warning(caution3, cx, cy)
+    
+    # 4º Quadrante
+    caution4 = img[(img_shape[0]//2):, (img_shape[1]//2): ] 
+    cx = 0
+    cy = img_shape[0]//2
+    caution4, I4 = objects.warning(caution1, cx, cy)
+    
+    
+    # # Juntar as imagens
+    # warning = np.zeros((img_shape[0], img_shape[1]), dtype=np.int8)
+    
+    # warning[:(img_shape[0]//2), (img_shape[1]//2): ] = caution1 # 1º Quadrante
+    # warning[:(img_shape[0]//2), :(img_shape[1]//2) ] = caution2 # 2º Quadrante
+    # warning[(img_shape[0]//2):, :(img_shape[1]//2) ] = caution3 # 3º Quadrante
+    # warning[(img_shape[0]//2):, (img_shape[1]//2): ] = caution4 # 4º Quadrante
+    # print(warning.shape)
+      
+    # Mostrar resultado     
     while True:
-        cv.imshow("Warning", objects)
+        cv.imshow("I1", caution1)
+        cv.setWindowTitle("I1", f"I1={I1:.2f}")
+        
+        cv.imshow("I2", caution2)
+        cv.setWindowTitle("I2", f"I2={I2:.2f}")
+        
+        cv.imshow("I3", caution3)
+        cv.setWindowTitle("I3", f"I3={I3:.2f}")
+        
+        cv.imshow("I4", caution4)
+        cv.setWindowTitle("I4", f"I4={I4:.2f}")
         
         # Pressione a tecla "q" para parar
         if cv.waitKey(1) == ord("q"):
--- webcam.py
+++ webcam.py
@@ -3,7 +3,6 @@ import time
 import depthmap
 import numpy as np
 import nearbyObjects
-import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     
@@ -11,10 +10,11 @@ if __name__ == "__main__":
     depth_estimator = depthmap.depthEstimator()
 
     # Inicializando WebCam
-    # Pegar video do celular usar como argumento: "http://192.168.0.15:4747/video"
+    # Pegar video do celular usar como argumento: "http://192.168.0.11:4747/video"
     # Video da WebCam do Not usar como argumento: 0, cv.CAP_DSHOW
     camera = cv.VideoCapture(0, cv.CAP_DSHOW)
     cv.namedWindow("Depth Map", cv.WINDOW_NORMAL)
+    cv.namedWindow("Imagem Original", cv.WINDOW_NORMAL)
     
     while True:
         # Ler um frame da camera
@@ -28,22 +28,67 @@ if __name__ == "__main__":
             img = cv.flip(img, 1)
             disparity = depth_estimator.estimatorDepthMap(img)
            
-            # Objetos Próximos
-            objects = nearbyObjects.nearby_objects()
-            objects, I = objects.warning(disparity) 
-            print("\nI =",I)
-            
             # Combinar imagem de entrada e depth map
-            img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-            combinedImg = np.hstack((img, disparity, objects))
+            #img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+            #combinedImg = np.hstack((img, disparity))
+            
+            # Objetos Próximos por Quagrante
+            objects = nearbyObjects.nearby_objects()
+            disparity_shape = disparity.shape
+            
+            # 1º Quadrante
+            caution1 = disparity[:(disparity_shape[0]//2), (disparity_shape[1]//2): ] 
+            cx = 0
+            cy = 0
+            caution1, I1 = objects.warning(caution1, cx, cy)
+            
+            # 2º Quadrante
+            caution2 = disparity[:(disparity_shape[0]//2), :(disparity_shape[1]//2) ] 
+            cx = disparity_shape[1]//2
+            cy = 0
+            caution2, I2 = objects.warning(caution2, cx, cy)
+            
+            # 3º Quadrante
+            caution3 = disparity[(disparity_shape[0]//2):, :(disparity_shape[1]//2) ] 
+            cx = disparity_shape[1]//2
+            cy = disparity_shape[0]//2
+            caution3, I3 = objects.warning(caution3, cx, cy)
+            
+            # 4º Quadrante
+            caution4 = disparity[(disparity_shape[0]//2): , (disparity_shape[1]//2): ] 
+            cx = 0
+            cy = disparity_shape[0]//2
+            caution4, I4 = objects.warning(caution4, cx, cy)
+            
+            
+            
             
             # Calculo do FPS
             end_time = time.time()
             fps = round(1/(end_time - start_time), 2)
-            print("FPS:",fps)
+            
             # Mostrar Imagem e FPS
-            cv.putText(combinedImg, (f"FPS: {fps}"), (660, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (163,138,255), 2)
-            cv.imshow("Depth Map", combinedImg)
+            # cv.putText(combinedImg, (f"FPS: {fps}"), (660, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (163,138,255), 2)
+            # cv.imshow("Depth Map", combinedImg)
+            
+            cv.putText(img, (f"FPS: {fps}"), (20, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (163,138,255), 2)
+            cv.imshow("Imagem Original", img)
+            
+            
+            cv.putText(disparity, (f"FPS: {fps}"), (20, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (163,138,255), 2)
+            cv.imshow("Depth Map", disparity)
+            
+            cv.imshow("I1", caution1)
+            cv.setWindowTitle("I1", f"I1={I1:.2f}")
+            
+            cv.imshow("I2", caution2)
+            cv.setWindowTitle("I2", f"I2={I2:.2f}")
+            
+            cv.imshow("I3", caution3)
+            cv.setWindowTitle("I3", f"I3={I3:.2f}")
+            
+            cv.imshow("I4", caution4)
+            cv.setWindowTitle("I4", f"I4={I4:.2f}")
             
             
         else:
